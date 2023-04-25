586d92a6f09b7d44a5bdda555085ce720a038b49
priyankakarale7 <110196022+priyankakarale7@users.noreply.github.com>
2023-04-25 11:11:50 +0530

    Update Lists.scala

--- Lists.scala
+++ Lists.scala
@@ -1,17 +1,30 @@
 package Demo
-
 object Lists {
 
   //lists are immutable in scala and array is mutable
  val mylist : List[Int] = List(1,2,3,4,7,8,5);
   val names : List[String] = List("max", "tom", "John");
 
-  println(0 :: mylist);
-  println(mylist);
-  println(names);
-  println(1 :: 5 :: 9 :: Nil);
-  println(mylist.head);
-  println(mylist.tail);
-  println(names.isEmpty);
-  println(mylist.reverse);
+  def main(args: Array[String]): Unit = {
+
+
+    println(0 :: mylist);
+    println(mylist);
+    println(names);
+    println(1 :: 5 :: 9 :: Nil);
+    println(mylist.head);
+    println(mylist.tail);
+    println(names.isEmpty);
+    println(mylist.reverse);
+
+    println(List.fill(5)(2));
+    println(mylist.max);
+
+    mylist.foreach(println);
+    var sum: Int = 0;
+    mylist.foreach(sum += _);
+
+    println(sum);
+
+  }
 }
