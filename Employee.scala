d4c9bc4243df7f9728ecef2bd8592f620b202282
priyankakarale7 <110196022+priyankakarale7@users.noreply.github.com>
2023-05-03 15:34:30 +0530

    Update Employee.scala

--- Employee.scala
+++ Employee.scala
@@ -2,7 +2,7 @@ package Demo
 import java.time.LocalDate
 import scala.io.Source
 
-Question
+//Question
 /*
 1. Read Employee from csv file with 4 column {empId, empName, empSal, empDob, empDoj}
 
@@ -11,12 +11,24 @@ Question
 3. Sort it based on empDoj
 
 4. Iterate and print the data
+
+
+1)Read the file
+
+2)display distinct records
+
+3)sorted records using DOJ and DOB
+
+4) highest , lowest, avg salary
+
+5) employee above avg and below avg salary
  */
+
 
 
 case class Employee(empId: Int, empName: String, empSal: Double, empDob: LocalDate, empDoj: LocalDate)
 
-  object Employee extends App{
+  object Employee{
 
 
 val source = Source.fromFile("/Users/a614734/Downloads/employees1.csv")
@@ -36,12 +48,57 @@ LocalDate.parse(row(4))
 ))
 
 // remove duplicate records based on empId
-val uniqueEmployees = employees.groupBy(_.empId).map(_._2.head).toList
+    def printAllEmp{
+      val uniqueEmployees = employees.groupBy(_.empId).map(_._2.head).toList
 
-// sort employees based on empDoj
-val sortedEmployees = uniqueEmployees.sortBy(_.empDoj)
+      // sort employees based on empDoj
+      val sortedEmployees = uniqueEmployees.sortBy(emp => (emp.empDoj, emp.empDob))
 
-// print employee data
-sortedEmployees.foreach(e => println(s"${e.empId}, ${e.empName}, ${e.empSal}, ${e.empDob}, ${e.empDoj}"))
+      // print employee data
+      sortedEmployees.foreach(e => println(s"${e.empId}, ${e.empName}, ${e.empSal}, ${e.empDob}, ${e.empDoj}"))
+    }
+    def highestSalEmp{ // find the employee with the highest salary
+      val highestSalaryEmployee = employees.maxBy(_.empSal)
 
+      // print employee data
+      println(s"${highestSalaryEmployee.empId}, ${highestSalaryEmployee.empName}, ${highestSalaryEmployee.empSal}, ${highestSalaryEmployee.empDob}, ${highestSalaryEmployee.empDoj}")
+    }
+
+
+    def lowestSalEmp{ // find the employee with the highest salary
+      val lowestSalaryEmployee = employees.minBy(_.empSal)
+
+      // print employee data
+      println(s"${lowestSalaryEmployee.empId}, ${lowestSalaryEmployee.empName}, ${lowestSalaryEmployee.empSal}, ${lowestSalaryEmployee.empDob}, ${lowestSalaryEmployee.empDoj}")
+    }
+
+    val sal_sum = employees.map(_.empSal).sum
+    val total_emp = employees.size.toDouble
+    val avg_sal = sal_sum / total_emp
+
+
+    def above_avg {
+      //employee above average salary
+
+      println(("Average salary of employees is " + avg_sal))
+      val above_avg_Employees = employees.filter(e => e.empSal > avg_sal);
+
+      above_avg_Employees.foreach(e => println(s"${e.empId}, ${e.empName}, ${e.empSal}, ${e.empDob}, ${e.empDoj}"))
+    }
+    //below avg emp
+    def below_avg{
+      val below_avg_Employees = employees.filter(e => e.empSal < avg_sal);
+
+      below_avg_Employees.foreach(e => println(s"${e.empId}, ${e.empName}, ${e.empSal}, ${e.empDob}, ${e.empDoj}"))
+    }
+
+
+    def main(args: Array[String]): Unit = {
+      //printAllEmp
+      highestSalEmp
+      lowestSalEmp
+      above_avg
+      println()
+      below_avg
+    }
   }
